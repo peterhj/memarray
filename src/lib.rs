21770241b9efc7194f273655da61ddca9b9ba9ab
Peter Jin <peterhaijin@gmail.com>
2018-05-22 17:38:12 -0700

    Add `Mem` trait. Add `Shape` trait. Add a flat view API. Change `RangeArguments` -> `RangeBounds`.

--- src/lib.rs
+++ src/lib.rs
@@ -17,6 +17,7 @@ limitations under the License.
 #![feature(allocator_api)]
 #![feature(collections_range)]
 #![feature(ptr_internals)]
+//#![feature(specialization)]
 
 extern crate arrayidx;
 extern crate byteorder;
@@ -29,8 +30,9 @@ use parking_lot::{RwLock};
 
 use std::alloc::{Alloc, Global};
 use std::cell::{RefCell};
-use std::collections::range::{RangeArgument};
+use std::marker::{PhantomData};
 use std::mem::{size_of};
+use std::ops::{RangeBounds};
 use std::ptr::{NonNull, null_mut, write_bytes};
 use std::rc::{Rc};
 use std::slice::{from_raw_parts, from_raw_parts_mut};
@@ -39,6 +41,25 @@ use std::sync::{Arc};
 pub mod ffi;
 pub mod linalg;
 pub mod serial;
+
+fn i2idx(i: isize, len: usize) -> usize {
+  let u = i as usize;
+  if u < len {
+    return u;
+  }
+  let u = (len as isize + i) as usize;
+  if u < len {
+    return u;
+  }
+  panic!();
+}
+
+pub trait Mem<T> where T: Copy {
+  unsafe fn as_ptr(&self) -> *const T;
+  unsafe fn as_mut_ptr(&mut self) -> *mut T;
+  fn as_slice(&self) -> &[T];
+  fn as_mut_slice(&mut self) -> &mut [T];
+}
 
 pub struct HeapMem<T> where T: Copy {
   buf:  *mut T,
@@ -71,20 +92,22 @@ impl<T> HeapMem<T> where T: Copy {
       phsz: phsz,
     }
   }
+}
 
-  pub unsafe fn as_ptr(&self) -> *const T {
+impl<T> Mem<T> for HeapMem<T> where T: Copy {
+  unsafe fn as_ptr(&self) -> *const T {
     self.buf
   }
 
-  pub unsafe fn as_mut_ptr(&self) -> *mut T {
+  unsafe fn as_mut_ptr(&mut self) -> *mut T {
     self.buf
   }
 
-  pub fn as_slice(&self) -> &[T] {
+  fn as_slice(&self) -> &[T] {
     unsafe { from_raw_parts(self.buf, self.len) }
   }
 
-  pub fn as_mut_slice(&mut self) -> &mut [T] {
+  fn as_mut_slice(&mut self) -> &mut [T] {
     unsafe { from_raw_parts_mut(self.buf, self.len) }
   }
 }
@@ -106,6 +129,12 @@ impl ZeroBits for isize {}
 impl ZeroBits for f16_stub {}
 impl ZeroBits for f32 {}
 impl ZeroBits for f64 {}
+
+pub trait Shape {
+  type Shape;
+
+  fn shape(&self) -> Self::Shape;
+}
 
 pub trait Array {
   type Idx: ArrayIndex;
@@ -136,30 +165,54 @@ pub trait BatchArray: Array {
   fn set_batch_size(&mut self, new_batch_sz: usize);
 }
 
-pub struct MemArray<Idx, T> where T: Copy {
+pub trait ZerosShape {
+  type Shape;
+
+  fn zeros(size: Self::Shape) -> Self where Self: Sized;
+}
+
+/*pub trait MemArrayZeros: Array {
+  fn zeros(size: Self::Idx) -> Self where Self: Sized;
+}
+
+pub trait MemBatchArrayZeros: Array {
+  fn zeros(size: Self::Idx, batch_size: usize) -> Self where Self: Sized;
+}*/
+
+pub struct MemArray<Idx, T, M=HeapMem<T>> where T: Copy, M: Mem<T> {
   size:     Idx,
   offset:   Idx,
   stride:   Idx,
-  mem:      HeapMem<T>,
+  mem:      M,
+  _mrk:     PhantomData<*mut T>,
 }
 
+pub type MemScalar<T>  = MemArray0d<T>;
 pub type MemArray0d<T> = MemArray<Index0d, T>;
 pub type MemArray1d<T> = MemArray<Index1d, T>;
 pub type MemArray2d<T> = MemArray<Index2d, T>;
 pub type MemArray3d<T> = MemArray<Index3d, T>;
 pub type MemArray4d<T> = MemArray<Index4d, T>;
 
-impl<Idx, T> MemArray<Idx, T> where Idx: ArrayIndex, T: ZeroBits + Copy {
+/*impl<Idx, T> MemArray<Idx, T> where Idx: ArrayIndex, T: ZeroBits + Copy {
   pub fn zeros(size: Idx) -> Self {
+  }
+}*/
+
+impl<Idx, T> ZerosShape for MemArray<Idx, T> where Idx: ArrayIndex, T: ZeroBits + Copy {
+  type Shape = Idx;
+
+  fn zeros(size: Self::Shape) -> Self {
     let stride = size.to_packed_stride();
     let mem = unsafe { HeapMem::<T>::alloc(size.flat_len()) };
     // The memory is uninitialized, zero it using memset.
-    unsafe { write_bytes(mem.buf, 0, mem.len) };
+    unsafe { write_bytes::<T>(mem.buf, 0, mem.len) };
     MemArray{
       size:     size,
       offset:   Idx::zero(),
       stride:   stride,
       mem:      mem,
+      _mrk:     PhantomData,
     }
   }
 }
@@ -222,6 +275,20 @@ impl<Idx, T> MemArray<Idx, T> where Idx: ArrayIndex, T: Copy {
       None
     }
   }
+
+  pub fn flat_view_mut<'a>(&'a mut self) -> Option<MemArrayViewMut1d<'a, T>> {
+    if self.is_packed() {
+      let flat_size = self.flat_size();
+      Some(MemArrayViewMut{
+        size:       flat_size,
+        offset:     0,
+        stride:     flat_size.to_packed_stride(),
+        mem:        &mut self.mem,
+      })
+    } else {
+      None
+    }
+  }
 }
 
 pub struct MemOuterBatchArray<Idx, T> where T: Copy {
@@ -263,7 +330,7 @@ pub struct MemArrayView<'a, Idx, T> where /*Idx: 'static,*/ T: Copy + 'static {
   size:     Idx,
   offset:   Idx,
   stride:   Idx,
-  mem:      &'a HeapMem<T>,
+  mem:      &'a Mem<T>,
 }
 
 pub type MemArrayView0d<'a, T> = MemArrayView<'a, Index0d, T>;
@@ -309,7 +376,7 @@ impl<'a, T> MemArrayView1d<'a, T> where T: Copy + 'static {
   }
 
   pub fn view<R>(self, r: R) -> MemArrayView<'a, usize, T>
-  where R: RangeArgument<usize>,
+  where R: RangeBounds<usize>,
   {
     let (start_idx, end_idx) = range2idxs_1d(r, self.size);
     let view_size = end_idx - start_idx;
@@ -325,8 +392,8 @@ impl<'a, T> MemArrayView1d<'a, T> where T: Copy + 'static {
 
 impl<'a, T> MemArrayView2d<'a, T> where T: Copy + 'static {
   pub fn view<R0, R1>(self, r0: R0, r1: R1) -> MemArrayView<'a, [usize; 2], T>
-  where R0: RangeArgument<usize>,
-        R1: RangeArgument<usize>,
+  where R0: RangeBounds<usize>,
+        R1: RangeBounds<usize>,
   {
     let (start_idx, end_idx) = range2idxs_2d(r0, r1, self.size);
     let view_size = end_idx.index_sub(&start_idx);
@@ -342,9 +409,9 @@ impl<'a, T> MemArrayView2d<'a, T> where T: Copy + 'static {
 
 impl<'a, T> MemArrayView3d<'a, T> where T: Copy + 'static {
   pub fn view<R0, R1, R2>(self, r0: R0, r1: R1, r2: R2) -> MemArrayView<'a, [usize; 3], T>
-  where R0: RangeArgument<usize>,
-        R1: RangeArgument<usize>,
-        R2: RangeArgument<usize>,
+  where R0: RangeBounds<usize>,
+        R1: RangeBounds<usize>,
+        R2: RangeBounds<usize>,
   {
     let (start_idx, end_idx) = range2idxs_3d(r0, r1, r2, self.size);
     let view_size = end_idx.index_sub(&start_idx);
@@ -360,10 +427,10 @@ impl<'a, T> MemArrayView3d<'a, T> where T: Copy + 'static {
 
 impl<'a, T> MemArrayView4d<'a, T> where T: Copy + 'static {
   pub fn view<R0, R1, R2, R3>(self, r0: R0, r1: R1, r2: R2, r3: R3) -> MemArrayView<'a, [usize; 4], T>
-  where R0: RangeArgument<usize>,
-        R1: RangeArgument<usize>,
-        R2: RangeArgument<usize>,
-        R3: RangeArgument<usize>,
+  where R0: RangeBounds<usize>,
+        R1: RangeBounds<usize>,
+        R2: RangeBounds<usize>,
+        R3: RangeBounds<usize>,
   {
     let (start_idx, end_idx) = range2idxs_4d(r0, r1, r2, r3, self.size);
     let view_size = end_idx.index_sub(&start_idx);
@@ -375,13 +442,19 @@ impl<'a, T> MemArrayView4d<'a, T> where T: Copy + 'static {
       mem:      self.mem,
     }
   }
+
+  pub fn squeeze(self, axis: isize, idx: usize) -> MemArrayView3d<'a, T> {
+    let axis = i2idx(axis, 4);
+    // TODO
+    unimplemented!();
+  }
 }
 
 pub struct MemArrayViewMut<'a, Idx, T> where T: Copy + 'static {
   size:     Idx,
   offset:   Idx,
   stride:   Idx,
-  mem:      &'a mut HeapMem<T>,
+  mem:      &'a mut Mem<T>,
 }
 
 pub type MemArrayViewMut0d<'a, T> = MemArrayViewMut<'a, Index0d, T>;
@@ -408,12 +481,23 @@ impl<'a, Idx, T> DenseArray for MemArrayViewMut<'a, Idx, T> where Idx: ArrayInde
   }
 }
 
+impl<'a, Idx, T> Into<MemArrayView<'a, Idx, T>> for MemArrayViewMut<'a, Idx, T> where Idx: ArrayIndex, T: Copy + 'static {
+  fn into(self) -> MemArrayView<'a, Idx, T> {
+    MemArrayView{
+      size:     self.size,
+      offset:   self.offset,
+      stride:   self.stride,
+      mem:      self.mem,
+    }
+  }
+}
+
 impl<'a, Idx, T> MemArrayViewMut<'a, Idx, T> where Idx: ArrayIndex, T: Copy + 'static {
   pub unsafe fn as_ptr(&self) -> *const T {
     self.mem.as_ptr().offset(self.flat_offset() as _)
   }
 
-  pub unsafe fn as_mut_ptr(&self) -> *mut T {
+  pub unsafe fn as_mut_ptr(&mut self) -> *mut T {
     self.mem.as_mut_ptr().offset(self.flat_offset() as _)
   }
 
@@ -444,7 +528,7 @@ impl<'a, T> MemArrayViewMut1d<'a, T> where T: Copy + 'static {
   }
 
   pub fn view_mut<R>(self, r: R) -> MemArrayViewMut<'a, usize, T>
-  where R: RangeArgument<usize>,
+  where R: RangeBounds<usize>,
   {
     let (start_idx, end_idx) = range2idxs_1d(r, self.size);
     let view_size = end_idx - start_idx;
@@ -460,8 +544,8 @@ impl<'a, T> MemArrayViewMut1d<'a, T> where T: Copy + 'static {
 
 impl<'a, T> MemArrayViewMut2d<'a, T> where T: Copy + 'static {
   pub fn view_mut<R0, R1>(self, r0: R0, r1: R1) -> MemArrayViewMut<'a, [usize; 2], T>
-  where R0: RangeArgument<usize>,
-        R1: RangeArgument<usize>,
+  where R0: RangeBounds<usize>,
+        R1: RangeBounds<usize>,
   {
     let (start_idx, end_idx) = range2idxs_2d(r0, r1, self.size);
     let view_size = end_idx.index_sub(&start_idx);
@@ -477,9 +561,9 @@ impl<'a, T> MemArrayViewMut2d<'a, T> where T: Copy + 'static {
 
 impl<'a, T> MemArrayViewMut3d<'a, T> where T: Copy + 'static {
   pub fn view_mut<R0, R1, R2>(self, r0: R0, r1: R1, r2: R2) -> MemArrayViewMut<'a, [usize; 3], T>
-  where R0: RangeArgument<usize>,
-        R1: RangeArgument<usize>,
-        R2: RangeArgument<usize>,
+  where R0: RangeBounds<usize>,
+        R1: RangeBounds<usize>,
+        R2: RangeBounds<usize>,
   {
     let (start_idx, end_idx) = range2idxs_3d(r0, r1, r2, self.size);
     let view_size = end_idx.index_sub(&start_idx);
@@ -495,10 +579,10 @@ impl<'a, T> MemArrayViewMut3d<'a, T> where T: Copy + 'static {
 
 impl<'a, T> MemArrayViewMut4d<'a, T> where T: Copy + 'static {
   pub fn view_mut<R0, R1, R2, R3>(self, r0: R0, r1: R1, r2: R2, r3: R3) -> MemArrayViewMut<'a, [usize; 4], T>
-  where R0: RangeArgument<usize>,
-        R1: RangeArgument<usize>,
-        R2: RangeArgument<usize>,
-        R3: RangeArgument<usize>,
+  where R0: RangeBounds<usize>,
+        R1: RangeBounds<usize>,
+        R2: RangeBounds<usize>,
+        R3: RangeBounds<usize>,
   {
     let (start_idx, end_idx) = range2idxs_4d(r0, r1, r2, r3, self.size);
     let view_size = end_idx.index_sub(&start_idx);
@@ -509,6 +593,12 @@ impl<'a, T> MemArrayViewMut4d<'a, T> where T: Copy + 'static {
       stride:   self.stride.clone(),
       mem:      self.mem,
     }
+  }
+
+  pub fn squeeze_mut(self, axis: isize, idx: usize) -> MemArrayViewMut3d<'a, T> {
+    let axis = i2idx(axis, 4);
+    // TODO
+    unimplemented!();
   }
 }
 
@@ -524,12 +614,4 @@ pub struct SharedMemArray<Idx, T> where T: Copy {
   offset:   Idx,
   stride:   Idx,
   mem:      Arc<RwLock<HeapMem<T>>>,
-}
-
-pub trait MemArrayZeros: Array {
-  fn zeros(size: Self::Idx) -> Self where Self: Sized;
-}
-
-pub trait MemBatchArrayZeros: Array {
-  fn zeros(size: Self::Idx, batch_size: usize) -> Self where Self: Sized;
 }*/
